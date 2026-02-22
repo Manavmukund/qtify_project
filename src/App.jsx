a89bba7e88ddf06ddbc6883ccfee4a812c5f4865
Manav Kumar <manavmukund7257@gmail.com>
2026-02-23 00:36:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { useState } from 'react'
+import './App.css'
+// import Button from './components/Button/Button';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+
+function App() {
+   return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+
+ 
+}
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+// src/components/Button/Button.jsx
+import React from 'react';
+import styles from './Button.module.css';
+
+const Button = ({ text = 'Give Feedback', onClick }) => {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
