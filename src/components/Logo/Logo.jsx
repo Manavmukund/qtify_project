5ab2d80cbe64ee9e5e3cbdc6c5c45216fe8affaf
Manav Kumar <manavmukund7257@gmail.com>
2026-03-14 19:31:57 +0530

    Added Section component, grid layout for albums, and updated App.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import LogoImage from "../../assets/Logo.png";
+import logo from "../../assets/Logo.png";
 
-export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} />;
+function Logo() {
+  return <img src={logo} alt="QTify Logo" />;
 }
+
+export default Logo;
